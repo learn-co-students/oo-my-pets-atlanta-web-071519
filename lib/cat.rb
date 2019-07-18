66b3cd426622c3c97983f292e3ff220f5ea1739f
github username <github email address>
2019-07-18 15:34:58 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,27 @@
+require "pry"
+
 class Cat
   # code goes here
+  attr_reader :name
+  attr_accessor :owner 
+  attr_accessor :mood 
+
+  @@all_cats = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner 
+    @mood = "nervous"
+    @@all_cats << self 
+    @owner.cats << self 
+  end 
+
+  def self.all
+    @@all_cats
+  end 
+
+  def remove_owner
+    remove_instance_variable(:@owner)
+  end
+
 end
